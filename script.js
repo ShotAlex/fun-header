49bbebb7338feb7ae146badde2767723cc84463d
ShotAlex <shot.aleks@gmail.com>
2021-03-03 16:08:52 +0300

    feat: change algorithm

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("header").style.top = "0";
-    } else {
-        document.getElementById("header").style.top = "-120px";
+const header = document.getElementById('header');
+
+let prevPos = window.pageYOffset;
+let delta = 70;
+
+window.onscroll = () => {
+    let curPos = window.pageYOffset;
+
+    if((Math.abs(prevPos - curPos)) >= delta) {
+
+        if(prevPos > curPos) {
+            header.style.top = '0';
+        } else {
+            header.style.top = '-8em';
+        }
+
+        prevPos = curPos
     }
-    prevScrollpos = currentScrollPos;
 }
