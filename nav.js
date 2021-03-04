623eb6ab5755e9aaee599e4af316c71fd78bc8a4
ShotAlex <shot.aleks@gmail.com>
2021-03-04 10:42:09 +0300

    feat: add full logic for header

--- nav.js
+++ nav.js
@@ -1,44 +1,35 @@
-const headerTop = document.getElementById('header-top')
-const menu = document.getElementById('menu')
+const headerTop = document.getElementById('header-top');
+const menu = document.getElementById('menu');
 
-let headerTopHeight = headerTop.offsetHeight
-let menuHeight = menu.offsetHeight
-let headerHeight = headerTopHeight + menuHeight
-
+let headerTopHeight = headerTop.offsetHeight;
+let menuHeight = menu.offsetHeight;
+let headerHeight = headerTopHeight + menuHeight;
 
 let prevPos = window.pageYOffset;
 let delta = 70;
+let dotShow = headerHeight;
 
 
 window.onscroll = () => {
-    let curPos = window.pageYOffset
+    let curPos = window.pageYOffset;
 
     if (headerTopHeight > curPos) {
-        menu.style.transform = '0s'
-        menu.style.position = 'static'
+        menu.style.position = 'static';
     } else if (curPos >= headerTopHeight && curPos < headerHeight) {
-        menu.style.transform = '0.2s'
-        menu.style.top = '0'
+        menu.style.top = '0';
+        menu.style.transform = 'translateY(0px)';
     } else {
-        menu.style.top = `${-menuHeight}px`
-        menu.style.position = 'fixed'
+        menu.style.position = 'fixed';
+        menu.style.top = `${0}px`;
 
         if(curPos < prevPos) {
-            menu.style.top = `0px`
+            if(dotShow - curPos > delta) {
+                menu.style.transform = 'translateY(0px)';
+            }
         } else {
-            menu.style.top = `${-menuHeight}px`
+            menu.style.transform = `translateY(-${menuHeight}px)`;
+            dotShow = curPos;
         }
-        prevPos = curPos
+        prevPos = curPos;
     }
-
-
-    // if(curPos <= (headerTopHeight + menuHeight++) ) {
-    //     menu.style.top = `${headerTopHeight - curPos - 1}px`
-    // } else if (curPos > prevPos) {
-    //     menu.style.top = `${-menuHeight}px`;
-    // } else {
-    //     menu.style.top = `0`;
-    // }
-    // prevPos = curPos
-
 }
