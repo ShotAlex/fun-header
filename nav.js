38c96c585ad9fd8d35079e01ce2ec1edbe7ffccf
ShotAlex <shot.aleks@gmail.com>
2021-03-05 13:19:39 +0300

    chore: took out property from JS to CSS class

--- nav.js
+++ nav.js
@@ -1,4 +1,6 @@
 const headerTop = document.getElementById('header-top');
+const header = document.getElementById('header');
+
 const menu = document.getElementById('menu');
 
 let headerTopHeight = headerTop.offsetHeight;
@@ -10,17 +12,31 @@ let delta = 70;
 let dotShow = headerHeight;
 
 
-window.onscroll = () => {
+window.onscroll = function() {
+    visibleMenu()
+    // window.requestAnimationFrame(visibleMenu)
+    // window.webkitRequestAnimationFrame(visibleMenu)
+}
+
+
+function visibleMenu() {
     let curPos = window.pageYOffset;
 
+
+    // top of the screen between Top Menu in Header
     if (headerTopHeight > curPos) {
-        menu.style.position = 'static';
-    } else if (curPos >= headerTopHeight && curPos < headerHeight) {
-        menu.style.top = '0';
+        menu.classList.remove('fixed')
+        header.style.paddingBottom = 0;
+
+    // top of the screen between Menu in Header
+    } else if (curPos > headerTopHeight && curPos <= headerHeight) {
         menu.style.transform = 'translateY(0px)';
+
+
+    // top of the screen on rest of the page
     } else {
-        menu.style.position = 'fixed';
-        menu.style.top = `${0}px`;
+        menu.classList.add('fixed')
+        header.style.paddingBottom = `${menuHeight}px`
 
         if(curPos < prevPos) {
             if(dotShow - curPos > delta) {
@@ -32,4 +48,33 @@ window.onscroll = () => {
         }
         prevPos = curPos;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TEST event onWheel
+// const fun = document.getElementById('fun')
+// let hOneSize = 20;
+
+// fun.onwheel = (e) => {
+//     hOneSize += e.deltaY;
+//     fun.style.fontSize = `${hOneSize}px`;
+// }
